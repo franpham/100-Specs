cbe225a6d16e284b653ffc2c48212ea0cbbdff0d
franpham <franpham@gmail.com>
2015-09-26 10:09:29 -1000

    declared rainbow object

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -163,6 +163,17 @@ var browsers = {
  *
  */
 
+var rainbow = { colors : {
+    red       : "#F0280A",
+    orange    : "#FF8800",
+    yellow    : "#FFDD00",
+    green     : "#51AB0C",
+    blue      : "#1593ED",
+    indigo    : "#5215ED",
+    violet    : "#A915ED"
+  },
+  isDouble : true
+};
 
 /* Step 14
  *
